d70f1ab7fef949b2554887f381c5f905e777c304
johnsuh01 <johnsuh01@hotmail.com>
2019-12-11 17:21:05 -0500

    Exercise-302

---
+++ src/main/java/com/example/demo/Springboot302ExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot302ExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot302ExerciseApplication.class, args);
+    }
+
+}
